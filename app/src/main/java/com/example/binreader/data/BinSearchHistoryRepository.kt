401a620fedfa22e69b2a2bf61bce365c98fd4b58
Kirill Zverev <vesnanervakh@gmail.com>
2025-06-28 21:43:05 +0300

    refactor: use BinInfo in search history repo instead of String (should be so by tech requirements)

--- app/src/main/java/com/example/binreader/data/BinSearchHistoryRepository.kt
+++ app/src/main/java/com/example/binreader/data/BinSearchHistoryRepository.kt
@@ -1,22 +1,58 @@
 package com.example.binreader.data
 
+import com.example.binreader.model.BinBank
+import com.example.binreader.model.BinCountry
+import com.example.binreader.model.BinInfo
 import kotlinx.coroutines.delay
 
 interface BinSearchHistoryRepository {
-    suspend fun getBinSearchHistory(): List<String>
+    suspend fun getBinSearchHistory(): List<BinInfo>
 }
 
 class MockBinSearchHistoryRepository: BinSearchHistoryRepository {
-    override suspend fun getBinSearchHistory(): List<String> {
+    override suspend fun getBinSearchHistory(): List<BinInfo> {
         delay(2000)
-        return listOf("4571 7360", "3642 6451")
+        return listOf(
+            BinInfo(
+                scheme = "visa",
+                type = "debit",
+                brand = "Visa/Dankort",
+                country = BinCountry(
+                    name = "Denmark",
+                    latitude = 56,
+                    longitude = 10
+                ),
+                bank = BinBank(
+                    name = "Jyske Bank",
+                    url = "www.jyskebank.dk",
+                    phone = "+4589893300",
+                    city = "Hjørring"
+                )
+            ),
+            BinInfo(
+                scheme = "visa",
+                type = "debit",
+                brand = "Visa/Dankort",
+                country = BinCountry(
+                    name = "Denmark",
+                    latitude = 56,
+                    longitude = 10
+                ),
+                bank = BinBank(
+                    name = "Jyske Bank",
+                    url = "www.jyskebank.dk",
+                    phone = "+4589893300",
+                    city = "Hjørring"
+                )
+            )
+        )
     }
 }
 
 
 // TODO: Implement local repo, provide local storage and get actual history
 class LocalBinSearchHistoryRepository: BinSearchHistoryRepository {
-    override suspend fun getBinSearchHistory(): List<String> {
+    override suspend fun getBinSearchHistory(): List<BinInfo> {
         TODO("Not yet implemented")
     }
 }
