388e5fbd4e6b0ac10b8c8afe54bbc3ca71577cc9
Kirill Zverev <vesnanervakh@gmail.com>
2025-06-30 22:16:41 +0300

    feat: basic layout of history screen

--- app/src/main/java/com/example/binreader/model/BinInfo.kt
+++ app/src/main/java/com/example/binreader/model/BinInfo.kt
@@ -1,6 +1,7 @@
 package com.example.binreader.model
 
 data class BinInfo(
+    val binNumber: String? = null,
     val scheme: String? = null,
     val type: String? = null,
     val brand: String? = null,
